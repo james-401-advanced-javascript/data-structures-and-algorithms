14c8f732941d3f2abf3d20330a771a00946a3818
James Dunn <jamesdu@zillowgroup.com>
2019-10-16 11:17:24 -0700

    Finished insertBefore and insertAfter functions

--- challenges/Data-Structures/ll-insertions/index.js
+++ challenges/Data-Structures/ll-insertions/index.js
@@ -4,12 +4,14 @@ let LinkedList = require('./lib/linked-list');
 
 let list = new LinkedList();
 
+list.insert('Node5');
 list.insert('Node3');
 list.insert('Node2');
 list.insert('Node1');
 list.insert('Node0');
 console.log(list.toString());
 
-list.append('Node4');
-list.append('Node5');
-console.log(list.toString().slice(-7, -1));
+list.insertAfter('Node3', 'Node4');
+console.log(list.toString());
+list.insertAfter('Node2', 'Node2.5');
+console.log(list.toString());
--- challenges/Data-Structures/ll-insertions/lib/linked-list.js
+++ challenges/Data-Structures/ll-insertions/lib/linked-list.js
@@ -9,6 +9,9 @@ class LinkedList {
 
     // insert = add to beginning of list
     insert(value) {
+        if (!value) {
+            throw Error;
+        }
         let node = new Node(value);
 
         // if no head
@@ -26,8 +29,67 @@ class LinkedList {
         return this;
     }
 
+    // add a new node with the given newValue immediately before the first value node
+    insertBefore(value, newVal) {
+        if (value === undefined || newVal === undefined) {
+            throw Error('Must inlude value and newValue');
+        }
+        let node = new Node(newVal);
+        let current = this.head;
+        // value = 4
+        // newVal = 3
+        // current = 2
+        // 1 --> 2 --> 4 --> 5
+        // 1 --> 2 -->
+        // loop through linked list
+        // if current.next.data === value
+        // then newVal.next = current.next
+        // and current.next = node;
+        while (current.next) {
+            if (current.next.data === value) {
+                node.next = current.next;
+                current.next = node;
+                break;
+            } else {
+                current = current.next;
+            }
+        }
+        return this;
+    }
+    // add a new node with the given newValue immediately after the first value node
+    insertAfter(value, newVal) {
+        if (value === undefined || newVal === undefined) {
+            throw Error('Must inlude value and newValue');
+        }
+        let node = new Node(newVal);
+        let current = this.head;
+        // value = 5
+        // newVal = 4
+        // current = 3
+        // 1 --> 2 --> 3 --> 5
+        // 1 --> 2 --> 3 -->
+        // evaluate off of current.next in case inserting at end of list (even though append does this)
+        // loop through linked list
+        // if current.next.data === value
+        // then node.next = current.next.next
+        // and current.next.next = node;
+        while (current.next) {
+            if (current.next.data === value) {
+                node.next = current.next.next;
+                current.next.next = node;
+                break;
+            } else {
+                current = current.next;
+            }
+        }
+        return this;
+    }
+
     // A function called includes which takes in a value and returns a boolean if that value exists in the linked list
     includes(value) {
+        if (!value) {
+            throw Error;
+        }
         let current = this.head;
         while (current.next) {
             if (current.data === value) {
@@ -45,6 +107,9 @@ class LinkedList {
 
     // Return the entire linked list in a string
     toString() {
+        if (!this.head) {
+            console.log('The Linked List is empty');
+        }
         let current = this.head;
         let response = 'Linked List: \n';
         while (current.next) {
@@ -55,6 +120,9 @@ class LinkedList {
     }
 
     append(value) {
+        if (!value) {
+            throw Error;
+        }
         let node = new Node(value);
 
         // If the list is empty, set the new
