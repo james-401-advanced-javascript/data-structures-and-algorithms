9e1e22a9cc1e444f24a7040258265f2c68002768
James Dunn <jamesdu@zillowgroup.com>
2019-10-23 14:40:45 -0700

    Updated previous challenges

--- challenges/Data-Structures/linkedList/__tests__/linked-list.test.js
+++ challenges/Data-Structures/linkedList/__tests__/linked-list.test.js
@@ -2,16 +2,10 @@
 
 let linkedList = require('../lib/linked-list');
 
-// Can successfully instantiate an empty linked list
-// Can properly insert into the linked list
-// The head property will properly point to the first node in the linked list
-// Can properly insert multiple nodes into the linked list
-// Will return true when finding a value within the linked list that exists
-// Will return false when searching for a value in the linked list that does not exist
-// Can properly return a collection of all the values that exist in the linked list
-
 describe('The functionality of a linked list', () => {
+
     let list = new linkedList();
+    // Challenge 05 - Linked List
     it('Can successfully instantiate an empty linked list', () => {
         expect(list).toBeDefined();
     });
--- challenges/Data-Structures/ll-insertions/__tests__/linked-list.test.js
+++ challenges/Data-Structures/ll-insertions/__tests__/linked-list.test.js
@@ -2,16 +2,10 @@
 
 let linkedList = require('../lib/linked-list');
 
-// Can successfully instantiate an empty linked list
-// Can properly insert into the linked list
-// The head property will properly point to the first node in the linked list
-// Can properly insert multiple nodes into the linked list
-// Will return true when finding a value within the linked list that exists
-// Will return false when searching for a value in the linked list that does not exist
-// Can properly return a collection of all the values that exist in the linked list
-
 describe('The functionality of a linked list', () => {
     let list = new linkedList();
+
+    // Challenge 05 - Linked List
     it('Can successfully instantiate an empty linked list', () => {
         expect(list).toBeDefined();
     });
@@ -19,6 +13,44 @@ describe('The functionality of a linked list', () => {
         let node = list.insert('Node0');
         expect(typeof node.head.data).toBe('string');
     });
+    it('The head property will properly point to the first node in the linked list', () => {
+        let newList = list;
+        newList.insert('Node3');
+        newList.insert('Node2');
+        newList.insert('Node1');
+        expect(list.head.data).toMatch('Node1');
+    });
+    it('Can properly insert multiple nodes into the linked list', () => {
+        let newList = list;
+        let num = 1;
+        newList.insert('Node3');
+        newList.insert('Node2');
+        newList.insert('Node1');
+        while (newList.includes(`Node${num}`)) {
+            num++;
+        }
+        expect(true).toBe(num > 1);
+    });
+    it('Will return true when finding a value within the linked list that exists', () => {
+        let newList = list;
+        newList.insert('Node1');
+        expect(newList.includes('Node1')).toBe(true);
+    });
+    it('Will return false when searching for a value in the linked list that does not exist', () => {
+        let newList = list;
+        newList.insert('Node1');
+        expect(newList.includes('Node10')).toBe(false);
+    });
+    it('Can properly return a collection of all the values that exist in the linked list', () => {
+        let newList = list;
+        newList.insert('Node3');
+        newList.insert('Node2');
+        newList.insert('Node1');
+        expect(newList.toString()).toBeDefined();
+        expect(typeof newList.toString()).toBe('string');
+    });
+
+    // Challenge 06 - Linked List Insertions
     it('Can properly append a node to the end of the list', () => {
         let newList = list;
         newList.insert('Node1');
@@ -65,41 +97,5 @@ describe('The functionality of a linked list', () => {
         newList.insert('Node1');
         newList.insertAfter('Node3', 'Node4');
         expect(newList.toString().slice(-7, -1)).toMatch('Node4');
-    });
-    it('The head property will properly point to the first node in the linked list', () => {
-        let newList = list;
-        newList.insert('Node3');
-        newList.insert('Node2');
-        newList.insert('Node1');
-        expect(list.head.data).toMatch('Node1');
-    });
-    it('Can properly insert multiple nodes into the linked list', () => {
-        let newList = list;
-        let num = 1;
-        newList.insert('Node3');
-        newList.insert('Node2');
-        newList.insert('Node1');
-        while (newList.includes(`Node${num}`)) {
-            num++;
-        }
-        expect(true).toBe(num > 1);
-    });
-    it('Will return true when finding a value within the linked list that exists', () => {
-        let newList = list;
-        newList.insert('Node1');
-        expect(newList.includes('Node1')).toBe(true);
-    });
-    it('Will return false when searching for a value in the linked list that does not exist', () => {
-        let newList = list;
-        newList.insert('Node1');
-        expect(newList.includes('Node10')).toBe(false);
-    });
-    it('Can properly return a collection of all the values that exist in the linked list', () => {
-        let newList = list;
-        newList.insert('Node3');
-        newList.insert('Node2');
-        newList.insert('Node1');
-        expect(newList.toString()).toBeDefined();
-        expect(typeof newList.toString()).toBe('string');
     });
 });
