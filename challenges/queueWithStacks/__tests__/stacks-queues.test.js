a28680429a6ea8e2c8d02673e5c489092dac0627
James Dunn <jamesdu@zillowgroup.com>
2019-11-03 19:37:44 -0800

    Finished testing

--- challenges/queueWithStacks/__tests__/stacks-queues.test.js
+++ challenges/queueWithStacks/__tests__/stacks-queues.test.js
@@ -1,7 +1,42 @@
 'use strict';
 
+const PseudoQueue = require('../lib/pseudoQueue');
 let Stack = require('../lib/stack');
 
+describe('The proper functionality of a psuedo queue', () => {
+    it('Can successfully enqueue into your pseudo queue', () => {
+        let queue = new PseudoQueue();
+        queue.enqueue(1);
+        expect(queue.stack1.top.data).toBe(1);
+    });
+    it('Can successfully enqueue multiple values into your queue', () => {
+        let queue = new PseudoQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        expect(queue.stack1.top.data).toBe(3);
+    });
+    it('Can successfully dequeue out of a queue the expected value', () => {
+        let queue = new PseudoQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        expect(queue.dequeue()).toBe(1);
+    });
+    it('Can successfully empty a queue after multiple dequeues', () => {
+        let queue = new PseudoQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        expect(queue.stack1.top).toBeNull();
+    });
+    it('Can successfully instantiate an empty queue', () => {
+        let queue = new PseudoQueue();
+        expect(queue).toBeDefined();
+    });
+});
 describe('The proper functionality of a stack', () => {
     it('Can successfully push onto a stack', () => {
         let stack = new Stack();
