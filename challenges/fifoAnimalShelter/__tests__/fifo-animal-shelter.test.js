8a24e365fa7b994fb78905011b56badf901cee45
James Dunn <jamesdu@zillowgroup.com>
2019-11-06 21:37:16 -0800

    Finished functionality and 3 tests

--- challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
+++ challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
@@ -7,14 +7,19 @@ const Cat = require('../lib/Cat');
 
 describe('The proper functionality of the Animal Shelter queue', () => {
     it('Can successfully enqueue a Dog', () => {
-        let newEntry = shelter.enqueue(new Dog('Fido'));
-        console.log(newEntry);
+        shelter.enqueue(new Dog('Fido'));
+        expect(shelter.front.data.constructor.name).toBe('Dog');
     });
     it('Can successfully enqueue a Cat', () => {
-        let newEntry = shelter.enqueue(new Cat('Maxine'));
-        console.log(newEntry);
+        shelter.enqueue(new Cat('Felix'));
+        expect(shelter.rear.data.constructor.name).toBe('Cat');
     });
-    it('Error handling when you try to enqueue something that is neither a Dog nor a Cat', () => {});
+    it('Error handling when you try to enqueue something that is neither a Dog nor a Cat', () => {
+        let testShelter = new AnimalShelter();
+        expect(testShelter.enqueue(new Array(4))).toBe(
+            'This queue is only for cats and dogs'
+        );
+    });
     it('Can successfully dequeue a Dog', () => {});
     it('Can successfully dequeue a Cat', () => {});
     it('Can successfully dequeue the front of the AnimalShelter queue when you pass no parameters to dequeue', () => {});
--- challenges/fifoAnimalShelter/lib/AnimalShelter.js
+++ challenges/fifoAnimalShelter/lib/AnimalShelter.js
@@ -10,10 +10,19 @@ class AnimalShelter {
 
     // push = add to beginning of list
     enqueue(value) {
+    // Error handling if value is undefined
         if (value === undefined) {
             console.error('Must pass in a value');
             return -1;
         }
+        // Error handling if object is not cat or dog
+        if (value.constructor.name !== 'Dog') {
+            if (value.constructor.name !== 'Cat') {
+                return 'This queue is only for cats and dogs';
+            }
+        }
+        
+
         let node = new Node(value);
 
         // if no front
@@ -32,17 +41,56 @@ class AnimalShelter {
     }
 
     // Dequeue and return the front of the queue
-    dequeue() {
+    dequeue(pref) {
     // If there is no front, that must mean that the queue is empty
     // return -1 if queue is empty
         if (!this.front) {
             console.log('The queue is empty');
             return -1;
         }
-        let temp = this.front;
-        this.front = this.front.next;
-        temp.next = null;
-        return temp.data;
+        if (!pref || this.front.data.constructor.name.toLowerCase() === pref) {
+            let temp = this.front;
+            this.front = this.front.next;
+            temp.next = null;
+            return temp.data;
+        } else {
+            //store current var
+            //store previous var
+            let current = this.front.next;
+            let previous = this.front;
+
+            while (current.next) {
+                // make current front.next
+                // if current.data is what I'm looking for
+                // temp = current;
+                // previous.next = current.next
+                // temp.next = null;
+                // run temp.print();
+                // return this
+                if (current.data.constructor.name.toLowerCase() === pref) {
+                    let temp = current;
+                    previous.next = current.next;
+                    temp.next = null;
+                    temp.data.print();
+                    return this;
+                } else {
+                    previous = current;
+                    current = current.next;
+                }
+            }
+            // If last item in list is one we're looking for, remove it
+            // then set the rear to the item before the last item
+            if (current.data.constructor.name.toLowerCase() === pref) {
+                let temp = current;
+                previous.next = current.next;
+                this.rear = previous;
+                temp.next = null;
+                temp.data.print();
+                return this;
+            }
+            // Return message if item is not in queue
+            return 'There are none of those animals in the queue';
+        }
     }
 }
 
