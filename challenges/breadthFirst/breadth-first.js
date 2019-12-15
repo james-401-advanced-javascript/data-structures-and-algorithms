bf0e92c4e862bed16ea2e5b540ea934ecbbb521f
James Dunn <jamesdu@zillowgroup.com>
2019-12-14 20:25:48 -0800

    Cleared eslint errors

--- challenges/breadthFirst/breadth-first.js
+++ challenges/breadthFirst/breadth-first.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-const BinaryTree = require("./binary-tree");
-const Node = require("./node");
-const tree = new BinaryTree("A");
+const BinaryTree = require('./binary-tree');
+const Node = require('./node');
+const tree = new BinaryTree('A');
 
-tree.root.left = new Node("B");
-tree.root.right = new Node("C");
-tree.root.left.left = new Node("D");
-tree.root.left.right = new Node("E");
-tree.root.right.left = new Node("F");
-tree.root.right.right = new Node("G");
+tree.root.left = new Node('B');
+tree.root.right = new Node('C');
+tree.root.left.left = new Node('D');
+tree.root.left.right = new Node('E');
+tree.root.right.left = new Node('F');
+tree.root.right.right = new Node('G');
 
 console.log(tree);
 
