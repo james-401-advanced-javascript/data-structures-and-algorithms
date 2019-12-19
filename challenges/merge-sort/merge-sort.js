29aab5adab4a4e57f253780f38c739cedcd779f0
James Dunn <jamesdu@zillowgroup.com>
2019-12-18 18:12:29 -0800

    Added UML

---
+++ challenges/merge-sort/merge-sort.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/*
+Write a function called mergeSort, which takes an array of numbers and returns a sorted array after using the Merge Sort algorithm. Do not mutate (change) the array given to you as a parameter.
+*/
+
+function mergeSort(arr, sIndx, mid, eIndx) {
+    let 
+}
